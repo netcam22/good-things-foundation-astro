0ea1ac24502e471a1f8656dc351483df3273108b
netcam22 <annettelesage@btinternet.com>
2024-01-19 16:48:08 +0000

    feat: implement pages

--- src/stores/navItems.js
+++ src/stores/navItems.js
@@ -1,7 +1,7 @@
 export const useNavItemsStore = (function () {
   const state = {
     menu: [
-      {id: 1, name: 'home', title: "Home"},
+      {id: 1, name: '', title: "Home"},
       {id: 2, name: 'what-to-do', title: "What To Do"},
       {id: 3, name: 'the-digital-divide', title: "The Digital Divide"},
       {id: 4, name: 'get-involved', title: "Get Involved"},
