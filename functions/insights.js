cb48e3917605cbd73832082bbfcc3d09470fb80a
netcam22 <annettelesage@btinternet.com>
2024-01-24 10:44:53 +0000

    feat: update response

--- functions/insights.js
+++ functions/insights.js
@@ -10,6 +10,16 @@ async function handleRequest(context) {
     const asset = await context.env.ASSETS.fetch(url);
     const assetText = await asset.text();
     const newAssetText = assetText.replace("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.", randomFact);
-    const response = new Response(newAssetText, asset);
-    return response;
+    const response = new Response(newAssetText, {
+        headers: {
+        "content-type": "text/html;charset=UTF-8",
+        },
+        status: 200,
+    });
+    if(response.status === 200) {
+        return response;
+    }
+    else {
+        return new Response(assetText, asset);
+    }
 }
